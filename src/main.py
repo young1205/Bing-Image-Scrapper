67a6a2647b77256f16e89b874b07cf82cdc52518
JULIA <juliacy@gmail.com>
2024-06-14 14:46:25 -0400

    reformat w black

--- src/main.py
+++ src/main.py
@@ -2,35 +2,45 @@
 """
 Github: https://github.com/young1205/Bing-Image-Scrapper
 """
-#Import libraries
+# Import libraries
 import os
 from BingImageScrapper import BingImageScraper
 from patch import webdriver_executable
 
 if __name__ == "__main__":
-    #Define file path
-    webdriver_path = os.path.normpath(os.path.join(os.getcwd(), 'webdriver', webdriver_executable()))
-    image_path = os.path.normpath(os.path.join(os.getcwd(), 'photos'))
+    # Define file path
+    webdriver_path = os.path.normpath(
+        os.path.join(os.getcwd(), "webdriver", webdriver_executable())
+    )
+    image_path = os.path.normpath(os.path.join(os.getcwd(), "photos"))
 
-    #Website used for scraping: 
-    search_site = 'bing'
+    # Website used for scraping:
+    search_site = "bing"
 
-    #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
-    search_keys= ['pug']
+    # Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
+    search_keys = ["pug"]
 
-    #Parameters
+    # Parameters
     number_of_images = 20
     headless = False
-    min_resolution=(0,0)
-    max_resolution=(9999,9999)
+    min_resolution = (0, 0)
+    max_resolution = (9999, 9999)
 
-    #Main program
+    # Main program
     for search_key in search_keys:
-        image_scrapper = BingImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
-        
+        image_scrapper = BingImageScraper(
+            webdriver_path,
+            image_path,
+            search_key,
+            number_of_images,
+            headless,
+            min_resolution,
+            max_resolution,
+        )
+
         image_urls = image_scrapper.find_image_urls()
-        print("image",image_urls)
+        print("image", image_urls)
         image_scrapper.save_images(image_urls)
-    
-    #Release resources    
+
+    # Release resources
     del image_scrapper
